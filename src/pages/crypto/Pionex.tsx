ed298723984aa54efa3b33fd44b751c7d3162214
mmm25002500 <marryhan25648@gmail.com>
2023-07-22 07:47:56 +0800

    fix: position_price not working

--- src/pages/crypto/Pionex.tsx
+++ src/pages/crypto/Pionex.tsx
@@ -146,9 +146,9 @@ const Pionex: React.FC = () => {
           </div>
 
           <div className="grid md:grid-cols-3 md:gap-6">       
-            {/* 輸入持倉均價 */}
+            {/* 持倉均價 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="text" name="now_price"  id="now_price" onChange={ (event) => handleTextChange('now_price', event)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                <input type="text" name="position_price"  id="position_price" onChange={ (event) => handleTextChange('position_price', event)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">持倉均價</label>
             </div>
 
