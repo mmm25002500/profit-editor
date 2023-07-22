508c5d0bf2d10f463fc755a862476eac3588642e
mmm25002500 <marryhan25648@gmail.com>
2023-07-23 01:28:17 +0800

    feat: Pionex Page Finished

--- src/components/CryptoPage/BasedPage.tsx
+++ src/components/CryptoPage/BasedPage.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useRef, useEffect, forwardRef, useImperativeHandle } from "react";
+import React, { useState, useRef, useEffect, forwardRef, useImperativeHandle, ChangeEvent } from "react";
 import html2canvas from "html2canvas";
 import { saveAs } from "file-saver";
+import QRCode from "react-qr-code";
 
 const content_data = {
   position_price: 0,
@@ -9,11 +10,21 @@ const content_data = {
   invitation_code: '',
   LongShort: '',
   times: 0,
-  crypto: ''
+  crypto: '',
+  IA: 0,
+  fee: 0.0001,
+	QrCode: {
+		pos1: 0,
+		pos2: 0,
+		width: 0,
+		height: 0,
+	}
 };
 
 interface Props {
 	drawWithText: any;
+	generateQRCodeAddr: any;
+	bg_img: string;
 }
 
 export interface Content {
@@ -23,12 +34,20 @@ export interface Content {
 	invitation_code: string;
 	LongShort: string;
 	times: number;
-	crypto: string;
+  crypto: string;
+  IA: number;
+  fee: number;
+	QrCode: {
+		pos1: number;
+		pos2: number;
+		width: number;
+		height: number;
+	}
 }
 
 export interface BasedPageRef {
 	handleDownload: () => void;
-	handleTextChange: (key: string, event: React.ChangeEvent<HTMLInputElement>) => void;
+	handleTextChange: (key: string, event: string | number | Object) => void;
 }
 
 const BasedPage = forwardRef((props: Props, ref) => {
@@ -36,11 +55,11 @@ const BasedPage = forwardRef((props: Props, ref) => {
 	const [imageLoaded, setImageLoaded] = useState(false); // 用於標記圖片是否加載完成
 	const canvasRef = useRef<HTMLCanvasElement>(null); // 用於獲取 canvas 元素的參考
 	const imageRef = useRef<HTMLImageElement>(null); // 用於獲取圖片的參考
-
+	const [invitation_addr, setInvitationAddr] = useState(''); // 用於標記圖片是否加載完成
 	// 初始化圖片，載入圖片。
 	useEffect(() => {
 		const image = new Image();
-		image.src = "/imgs/crypto/pionex_clear.png";
+		image.src = props.bg_img;
 		image.onload = () => {
 			setImageLoaded(true);
 		};
@@ -48,6 +67,7 @@ const BasedPage = forwardRef((props: Props, ref) => {
 
 	// 當圖片已載入，或 context 值改變的時候，預覽就改變。
 	useEffect(() => {
+		setInvitationAddr(props.generateQRCodeAddr(content.invitation_code));
 		preview();
 	}, [imageLoaded, content]);
 
@@ -66,6 +86,17 @@ const BasedPage = forwardRef((props: Props, ref) => {
 			// ctx?.canvas.width = 0;
 			// ctx?.canvas.height = 0;
 
+			// 繪製 QR Code;
+			const img = new Image();
+			const svg = document.getElementById("QRCode");
+			if (svg) {
+				const svgData = svg.outerHTML;
+				img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+			}
+			img.onload = () => {
+				ctx?.drawImage(img, 500, 500, 500, 500);
+			};
+
 			// 增加文字
 			if (ctx) {
 				props.drawWithText(ctx, content, 2);
@@ -76,11 +107,12 @@ const BasedPage = forwardRef((props: Props, ref) => {
 
 			// 使用 file-saver 將圖片數據保存為圖片文件
 			saveAs(imgData, "modified_image.png");
+			console.log("下載圖片");
 		}
 	};
 
-	const handleTextChange = (key: string, event: React.ChangeEvent<HTMLInputElement>) => {
-		setContent({ ...content, [key]: event.target.value });
+	const handleTextChange = (key: string, value: string | number | Object) => {
+		setContent({ ...content, [key]: value });
   };
 
 	// 使用 useImperativeHandle 設置 handleDownload 函數以便透過 ref 呼叫
@@ -88,7 +120,6 @@ const BasedPage = forwardRef((props: Props, ref) => {
 		handleDownload: handleDownload, // 將 handleDownload 函數暴露給父層
 		handleTextChange: handleTextChange, // 將 handleTextChange 函數暴露給父層
 	}));
-	
 
 	// 繪製圖片並加入文字
 	const preview = () => {
@@ -108,13 +139,35 @@ const BasedPage = forwardRef((props: Props, ref) => {
 				props.drawWithText(ctx, content, 2);
 			}
 		}
+
+		// 繪製 QR Code;
+		const img = new Image();
+		const svg = document.getElementById("QRCode");
+		if (svg) {
+			const svgData = svg.outerHTML;
+			img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+		}
+		img.onload = () => {
+			ctx?.drawImage(img, content.QrCode.pos1, content.QrCode.pos2, content.QrCode.width, content.QrCode.height);
+		};
 	};
-	
+
 	return (
 		<>
+			{
+				invitation_addr ? (
+					<QRCode
+						id="QRCode"
+						value={invitation_addr}
+						className="hidden"
+					/>
+				): (
+						<></>
+				)
+			}
 			<img
 				ref={imageRef}
-				src="/imgs/crypto/pionex_clear.png"
+				src={ props.bg_img }
 				alt="Image with Text"
 				style={{ display: "none" }}
 			/>
@@ -133,12 +186,12 @@ BasedPage.displayName = "BasedPage";
 
 export default BasedPage;
 
-export const calculator = (LongShort: string, multiple: number, position_price: number, now_price: number): number => {
+export const calculator = (LongShort: string, multiple: number, position_price: number, now_price: number, IA: number, fee: number): number => {
 	if (LongShort === 'L') {
-		return (now_price - position_price) / position_price * 100 * multiple;
+		return (now_price - position_price) * multiple * IA * (1-fee) / position_price;
 	} else if (LongShort === 'S') {
-		return (position_price - now_price) / position_price * 100 * multiple;
-	} else { 
+		return (position_price - now_price) * multiple * IA * (1-fee) / now_price;
+	} else {
 		return 0;
 	}
 }
--- src/pages/crypto/Pionex.tsx
+++ src/pages/crypto/Pionex.tsx
@@ -5,49 +5,51 @@ import Head from "next/head";
 import PageSelector from "@/components/CryptoPage/PageSelector";
 import BasedPage, { calculator, BasedPageRef, Content } from "@/components/CryptoPage/BasedPage";
 import Dropdown from "@/components/CryptoPage/PageSelector";
-
-const content_data = {
-  position_price: 0,
-  now_price: 0,
-  position_time: '',
-  invitation_code: '',
-  LongShort: '',
-  times: 0,
-  crypto: ''
-};
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
 
 const Pionex: React.FC = () => {
-  const [imageLoaded, setImageLoaded] = useState(false); // 用於標記圖片是否加載完成
-  const canvasRef = useRef<HTMLCanvasElement>(null); // 用於獲取 canvas 元素的參考
-  const imageRef = useRef<HTMLImageElement>(null); // 用於獲取圖片的參考
 
-  // Create a ref using useRef
+  // 建立 ref
   const basedPageRef = useRef<BasedPageRef>(null);
 
-  // 初始化圖片，載入圖片。
-  useEffect(() => {
-    const image = new Image();
-    image.src = "/imgs/crypto/pionex_clear.png";
-    image.onload = () => {
-      setImageLoaded(true);
-    };
-  }, []);
-
-
-  // Parent component's handleDownload function
+  // 處理下載，子組件的方法傳遞上來
   const handleDownload = () => {
     basedPageRef.current?.handleDownload();
   };
 
-  // Parent component's handleTextChange function
-  const handleTextChange = (key: string, event: React.ChangeEvent<HTMLInputElement>) => {
-    basedPageRef.current?.handleTextChange(key, event);
+  // 處理文字，子組件的方法傳遞上來
+  const handleTextChange = (key: string, value: string | number | Object) => {
+    basedPageRef.current?.handleTextChange(key, value);
+  };
+
+  // 千分位逗號，小數點不逗號
+  const numberWithCommas = (x: number) => {
+    const parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+  };
+
+  useEffect(() => {
+    // 設定 QR Code
+    handleTextChange('QrCode', { pos1: 100, pos2: 892, width: 149, height: 149 });
+    // 設定手續費
+    handleTextChange('fee', 0.001);
+  }, []);
+
+  // 產生 QR Code
+  const generateQRCodeAddr = (invitation_code: string): string => {
+    if (invitation_code)
+      return "https://www.pionex.com/zh-TW/sign/ref/" + invitation_code;
+    else
+      return '';
   };
 
   // 繪製文字
-  const drawWithText = (ctx: CanvasRenderingContext2D, content: Content, times: number=1) => {
+  const drawWithText = (ctx: CanvasRenderingContext2D, content: Content, times: number = 1) => {
+
     // %數
-    const percent = calculator(content.LongShort, content.times, content.position_price, content.now_price);
+    const percent = calculator(content.LongShort, content.times, content.position_price, content.now_price, content.IA, content.fee);
     if (percent >= 0) {
       ctx.font = `${62*times}px youhei`;
       ctx.fillStyle = "#009e64";
@@ -57,14 +59,19 @@ const Pionex: React.FC = () => {
       ctx.fillStyle = "#d03939";
       ctx.textAlign = "left";
     }
-    ctx.fillText((Math.round(percent * 100)/100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")+'%', 49*times, 248*times);
+
+    if (Math.round(percent * 100) / 100 > 0) {
+      ctx.fillText( "+"+ numberWithCommas(Math.round(percent * 100)/100).toString()+'%', 49*times, 248*times);
+    } else {
+      ctx.fillText(numberWithCommas(Math.round(percent * 100)/100).toString()+'%', 49*times, 248*times);
+    }
 
     // 幣種
     ctx.font = `bold ${18*times}px inter`;
     ctx.fillStyle = "white";
     ctx.textAlign = "left";
     ctx.fillText(content.crypto + ' 永續合約', 140*times, 160*times);
-    
+
     // 倍數
     ctx.font = `bold ${18*times}px inter`;
     ctx.fillStyle = "white";
@@ -89,21 +96,33 @@ const Pionex: React.FC = () => {
     ctx.font = `${14*times}px inter`;
     ctx.fillStyle = "white";
     ctx.textAlign = "left";
-    ctx.fillText(content.position_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), 47*times, 332*times);
+    ctx.fillText(numberWithCommas(content.position_price).toString(), 47*times, 332*times);
 
     // 標記價格
     ctx.font = `${14*times}px inter`;
     ctx.fillStyle = "white";
     ctx.textAlign = "left";
-    ctx.fillText(content.now_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), 175*times, 332*times);
+    ctx.fillText(numberWithCommas(content.now_price).toString(), 175*times, 332*times);
 
     // 持有時間
     ctx.font = `${14*times}px inter`;
     ctx.fillStyle = "white";
     ctx.textAlign = "left";
-    ctx.fillText(content.position_time, 303*times, 332*times);
+    // 兩個時間之間加上空格
+    if (content.position_time.includes('天') && content.position_time.includes('小時')) {
+      const text = content.position_time.split('天')[0] + '天 ' + content.position_time.split('天')[1].split('小時')[0] + '小時';
+      ctx.fillText(text, 303 * times, 332 * times);
+    } else if (content.position_time.includes('小時') && content.position_time.includes('分')) {
+      const text = content.position_time.split('小時')[0] + '小時 ' + content.position_time.split('小時')[1].split('分')[0] + '分';
+      ctx.fillText(text, 303 * times, 332 * times);
+    } else if (content.position_time.includes('分') && content.position_time.includes('秒')) {
+      const text = content.position_time.split('分')[0] + '分 ' + content.position_time.split('分')[1].split('秒')[0] + '秒';
+      ctx.fillText(text, 303 * times, 332 * times);
+    } else {
+      ctx.fillText(content.position_time, 303 * times, 332 * times);
+    }
 
-    // 持有時間
+    // 邀請碼
     ctx.font = `bold ${25*times}px inter`;
     ctx.fillStyle = "white";
     ctx.textAlign = "left";
@@ -119,8 +138,12 @@ const Pionex: React.FC = () => {
         {/* 顯示圖片 */}
         <h2 className="text-2xl sm:text-3xl">Pionex 永續合約獲利單編輯器</h2>
         <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-
-        <BasedPage ref={basedPageRef} drawWithText={drawWithText}></BasedPage>
+        <BasedPage
+          ref={basedPageRef}
+          drawWithText={drawWithText}
+          generateQRCodeAddr={generateQRCodeAddr}
+          bg_img="/imgs/crypto/pionex_clear.png"
+        ></BasedPage>
 
         <form>
           <Dropdown currentExchange="Pionex"></Dropdown>
@@ -128,55 +151,56 @@ const Pionex: React.FC = () => {
           <div className="grid md:grid-cols-3 md:gap-6">
             {/* 輸入幣種 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="text" name="crypto" id="crypto" onChange={ (event) => handleTextChange('crypto', event)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                <input type="text" name="crypto" id="crypto" onChange={ (event ) => handleTextChange('crypto', event.target.value )} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">幣種</label>
             </div>
 
             {/* 輸入倍數 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="number" name="times" onChange={ (event) => handleTextChange('times', event)} id="times" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                <input type="number" name="times" onChange={ (event) => handleTextChange('times', event.target.value)} id="times" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">倍數</label>
             </div>
 
             {/* 輸入方向 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="text" name="LongShort" onChange={ (event) => handleTextChange('LongShort', event)} id="LongShort" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                <input type="text" name="LongShort" onChange={ (event) => handleTextChange('LongShort', event.target.value)} id="LongShort" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">方向(多: L, 空: S)</label>
             </div>
           </div>
 
-          <div className="grid md:grid-cols-3 md:gap-6">       
+          <div className="grid md:grid-cols-3 md:gap-6">
             {/* 持倉均價 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="text" name="position_price"  id="position_price" onChange={ (event) => handleTextChange('position_price', event)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                <input type="text" name="position_price"  id="position_price" onChange={ (event) => handleTextChange('position_price', event.target.value)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">持倉均價</label>
             </div>
 
             {/* 標記價格 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="number" name="now_price" onChange={ (event) => handleTextChange('now_price', event)} id="now_price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <input type="number" name="now_price" onChange={ (event) => handleTextChange('now_price', event.target.value)} id="now_price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">標記價格</label>
             </div>
 
-            {/* 持有時間 */}
+            {/* 倉位價值 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="text" onChange={ (event) => handleTextChange('position_time', event)} name="position_time" id="position_time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">持有時間</label>
+                <input type="number" name="IA" onChange={ (event) => handleTextChange('IA', event.target.value)} id="IA" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">倉位價值</label>
             </div>
           </div>
 
           <div className="grid md:grid-cols-2 md:gap-6">
+            {/* 持有時間 */}
+            <div className="relative z-0 w-full mb-6 group">
+                <input type="text" onChange={ (event) => handleTextChange('position_time', event.target.value)} name="position_time" id="position_time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">持有時間</label>
+            </div>
+
             {/* 輸入邀請碼 */}
             <div className="relative z-0 w-full mb-6 group">
-                <input type="text" name="invitation_code" id="invitation_code" onChange={ (event) => handleTextChange('invitation_code', event)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <input type="text" name="invitation_code" id="invitation_code" onChange={ (event) => handleTextChange('invitation_code', event.target.value)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">邀請碼</label>
             </div>
 
-            {/* 上傳QR Code */}
-            <div className="relative z-0 w-full mb-6 group">
-                <input type="text" name="invitation_code" id="invitation_code" onChange={ (event) => handleTextChange('df', event)} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">上傳QR Code</label>
-            </div>
           </div>
           <button onClick={handleDownload} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">下載圖片</button>
         </form>
